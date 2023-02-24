48bfa83e666edc0396feb8f2c211959a9b3ce936
Leonidez A. Rosado <2020152397@ub.edu.bz>
2023-02-24 09:19:24 -0600

    interfaces2

--- q1/main.go
+++ q1/main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 )
 
+//create an interface named Salary
+//in the interface create a signiture method named monthly_salary and returns a float64
 type salary interface {
 	monthly_salary() float64
 }
 
+//struct for the interface
+//struct type for the google employee
 type Google_employee struct {
 	hours float64
  	days float64
@@ -15,6 +19,8 @@ type Google_employee struct {
 	weeks int
 }
 
+//struct for the interface
+//struct type for the netflix employee
 type Netflix_employee struct {
 	hours float64
  	days float64
@@ -22,6 +28,7 @@ type Netflix_employee struct {
 	weeks int
 }
 
+//use the struct type we created to implement the monthly_salary() of interface
 func (g Google_employee) monthly_salary() float64 {
 	return ((g.hours * g.hourly_pay) * g.days) * float64(g.weeks)
 }
@@ -30,7 +37,10 @@ func (n Netflix_employee) monthly_salary() float64 {
 	return ((n.hours * n.hourly_pay) * n.days) * float64(n.weeks)
 }
 
+//main function
 func main() {
+
+	//assign values to the types we created
 	employee1 := Google_employee {
 		hours: 10,
 		days: 4,
@@ -45,6 +55,7 @@ func main() {
 		weeks: 4,
 	}
 
+	//print out the result
 	fmt.Println("the monthly salary for employee1: ",employee1.monthly_salary())
 	fmt.Println("the monthly salary for employee2: ",employee2.monthly_salary())
 }
