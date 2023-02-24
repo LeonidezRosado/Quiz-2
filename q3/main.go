003e4147782012f43aa3c24e890597864735bc28
Leonidez A. Rosado <2020152397@ub.edu.bz>
2023-02-24 09:23:59 -0600

    comments

--- q3/main.go
+++ q3/main.go
@@ -5,6 +5,9 @@ import (
 	"time"
 )
 
+//creat function named user 
+//print out the string that is passed in 3 times 
+//use the sleep function here to delay for 100 milliseconds
 func user(s string) {
 	for i := 0; i < 3; i++ {
 		fmt.Println(s)
@@ -13,8 +16,10 @@ func user(s string) {
 }
 
 func main() {
+	//by writing go infront of the function call it is now a goroutine
 	go user("first")
 	 user("second")
 
+	 //use time function so goroutine can run 
 	time.Sleep(time.Second)
 }
